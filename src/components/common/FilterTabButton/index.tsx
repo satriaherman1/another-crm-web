93977c0d11c23df2d8606ef7bcfa69a56311cd52
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-21 11:01:13 +0700

    optimize properties tab

--- src/components/common/FilterTabButton/index.tsx
+++ src/components/common/FilterTabButton/index.tsx
@@ -3,7 +3,7 @@ import "./styles.scss";
 export default function FilterTabButton(props: ITabFilter) {
   const { tabButton, className } = props;
   return (
-    <div className={`${className ?? ""} filter-tab-button`}>
+    <div className={`${className ?? ""} flex  filter-tab-button`}>
       {tabButton.map((btn) => (
         <button onClick={btn.onClick} className={`${btn.activeIndicator === btn.key && "active"}`}>
           {btn.key}
--- src/components/pages/dashboard/settings/property/index.tsx
+++ src/components/pages/dashboard/settings/property/index.tsx
@@ -106,7 +106,7 @@ export default function SettingsProperty() {
       <p className="my-8">
         Properties store information about a contact, company, deal, or ticket. Your HubSpot account includes a number of contact, company, deal, and ticket properties by default.
       </p>
-      <div className="flex flex-col lg:flex-row justify-between w-full my-4 lg:items-center">
+      <div className="flex flex-col md:flex-row justify-between w-full my-4 lg:items-center">
         <FilterTabButton tabButton={filterList} />
         <Button variant="blue" className="mt-9 lg:mt-0">
           Create Properties
