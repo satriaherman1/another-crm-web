fa4d6276e8b5e0587e24c0bb742d89f9ca4ecdb5
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-24 11:55:10 +0700

    fix wording

--- src/components/common/CallKeyboard/index.tsx
+++ src/components/common/CallKeyboard/index.tsx
@@ -28,45 +28,45 @@ export default function CallKeyboard(props: ICallKeyboard) {
       <section className="flex gap-x-20">
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "4")}>
           <span className="text-[24px]">4</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal ">GHI</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "5")}>
           <span className="text-[24px]">5</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal ">JKL</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "6")}>
           <span className="text-[24px]">6</span>
-          <span className="text-crm-gray-600 font-normal ">DEF</span>
+          <span className="text-crm-gray-600 font-normal ">MNO</span>
         </button>
       </section>
 
       <section className="flex gap-x-20">
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "7")}>
           <span className="text-[24px]">7</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal ">PQRS</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "8")}>
           <span className="text-[24px]">8</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal ">TUV</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "9")}>
           <span className="text-[24px]">9</span>
-          <span className="text-crm-gray-600 font-normal ">DEF</span>
+          <span className="text-crm-gray-600 font-normal ">WXYZ</span>
         </button>
       </section>
 
       <section className="flex gap-x-20">
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "*")}>
           <span className="text-[24px]">*</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal invisible">ABC</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "0")}>
           <span className="text-[24px]">0</span>
-          <span className="text-crm-gray-600 font-normal ">ABC</span>
+          <span className="text-crm-gray-600 font-normal invisible">ABC</span>
         </button>
         <button className="flex flex-col items-center gap-y-1" onClick={(state) => changeFunc(targetValue + "#")}>
           <span className="text-[24px]">#</span>
-          <span className="text-crm-gray-600 font-normal ">DEF</span>
+          <span className="text-crm-gray-600 font-normal invisible">DEF</span>
         </button>
       </section>
 
